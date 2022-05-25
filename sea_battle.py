694a30423fec4c4640cacbee509728e2701f7f3b
Maksim Glazunov <magus.mgl@gmail.com>
2022-05-25 13:06:33 +0500

    Добавил методы display_board и out для класса Board

--- sea_battle.py
+++ sea_battle.py
@@ -16,12 +16,8 @@ class CheckCoordDort:
         return getattr(instance, self.name)
 
     def __set__(self, instance, value):
-        try:
-            self.validate_coord(value)
-        except DortCordsException as e:
-            print(e)
-        else:
-            setattr(instance, self.name, value)
+        self.validate_coord(value)
+        setattr(instance, self.name, value)
 
 
 class Dot:
@@ -36,87 +32,100 @@ class Dot:
         return self.x == other.x and self.y == other.y
 
 
+
 class Ship:
     "в конструктор передаём информацию о его положении на доске\
     кораблей: 1 корабль на 3 клетки, 2 корабля на 2 клетки, 4 корабля на одну клетку."
 
     def __init__(self, length_ship: int, start_dot: object, ship_direction: str, number_of_lives: int):
-        try:
-            self.validate_length_ship(length_ship)
-            self.validate_num_lives(number_of_lives)
-            self.validate_direction_ship(ship_direction)
-        except ShipLengthException as e:
-            print(e)
-        except ShipLivesException as e:
-            print(e)
-        except ShipDirectionException as e:
-            print(e)
-        else:
-            self.length_ship = length_ship
-            self.start_dot = start_dot
-            self.ship_direction = ship_direction
-            self.number_of_lives = number_of_lives
+        self.validate_length_ship(length_ship)
+        self.validate_num_lives(number_of_lives)
+        self.validate_direction_ship(ship_direction)
+
+        self.length_ship = length_ship
+        self.start_dot = start_dot
+        self.ship_direction = ship_direction
+        self.number_of_lives = number_of_lives
 
     @classmethod
-    def validate_length_ship(cls, arg):
-        if not isinstance(arg, int) or not 1 <= arg <= 3:
-            raise ShipLengthException(arg)
+    def validate_length_ship(cls, value: int) -> None:
+        """Проверяет значение длины корaбля (число от 1 до 3)"""
+        if not isinstance(value, int) or not 1 <= value <= 3:
+            raise ShipLengthException(value)
 
     @classmethod
-    def validate_num_lives(cls, arg):
-        if not isinstance(arg, int) or not 0 <= arg <= 3:
-            raise ShipLivesException(arg)
+    def validate_num_lives(cls, value: int) -> None:
+        """Проверяет значение жизней коробля (число от 0 до 3)"""
+        if not isinstance(value, int) or not 0 <= value <= 3:
+            raise ShipLivesException(value)
 
     @classmethod
-    def validate_direction_ship(cls, arg: str):
-        if arg != "v" and arg != "h":
-            raise ShipDirectionException(arg)
+    def validate_direction_ship(cls, value: str) -> None:
+        """Проверяет значение направление корабля ('v' и 'h')"""
+        if value != "v" and value != "h":
+            raise ShipDirectionException(value)
 
     @property
-    def dots(self):
+    def dots(self) -> list:
         """Возвращает список всех точек коробля"""
         list_of_dots = []
         for i in range(self.length_ship):
             if self.ship_direction == "v":
-                list_of_dots.append((self.start_dot.x + i, self.start_dot.y))
+                list_of_dots.append(Dot(self.start_dot.x + i, self.start_dot.y))
             else:
-                list_of_dots.append((self.start_dot.x, self.start_dot.y + i))
+                list_of_dots.append(Dot(self.start_dot.x, self.start_dot.y + i))
         return list_of_dots
 
 
 class Board:
-    def __init__(self, list_of_ships: list, number_of_live_ships, hid=False):
+    _hid = None
+
+    def __init__(self, list_of_ships: list, number_of_live_ships: int, hid: bool):
         self.board = [[" O "] * 6 for _ in range(6)]
         self.list_of_ships = list_of_ships
         self.number_of_live_ships = number_of_live_ships
         self.hid = hid
 
+    @property
+    def hid(self):
+        return self._hid
+
+    @hid.setter
+    def hid(self, value):
+        if isinstance(value, bool):
+            self._hid = value
+
     def add_ship(self, ship: object):
-        """ ставит корабль на доску (если ставить не получается, выбрасываем исключения)"""
-        for x, y in ship.dots:
-            if any([x > len(self.board) - 1, y > len(self.board[0]) - 1]):
-                raise BoardOutException(x, y)
-            if self.board[x][y] != " O ":
-                raise BoardOccupiedCage(x, y)
-            self.board[x][y] = " ■ "
+        """Ставит корабль на доску (если ставить не получается, выбрасываем исключения)"""
+        for dot in ship.dots:
+            if not self.out(dot):
+                raise BoardOutException(dot.x, dot.y)
+            if self.board[dot.x][dot.y] != " O ":
+                raise BoardOccupiedCage(dot.x, dot.y)
+            self.board[dot.x][dot.y] = " ■ "
 
     def contour(self):
         """который обводит корабль по контуру. Он будет полезен и в ходе самой игры,
         и в при расстановке кораблей (помечает соседние точки, где корабля по правилам быть не может)."""
 
     def display_board(self):
-        """выводит доску в консоль в зависимости от параметра hid"""
-        print("  | 1 | 2 | 3 | 4 | 5 | 6|")
+        """ Выводит доску в консоль в зависимости от параметра hid"""
+        print(" | 0 | 1 | 2 | 3 | 4 | 5 |")
         print("-" * 25)
-        if self.hid == False:
+        if self.hid:
+            for i, row in enumerate(self.board):
+                print(f"{i}| {'|'.join([' O '] * 6)}|")
+                print("-" * 25)
+        else:
             for i, row in enumerate(self.board):
                 print(f"{i}|{'|'.join(row)}|")
                 print("-" * 25)
 
-    def out(self, dort):
+    def out(self, dot: object):
         """для точки (объекта класса Dot) возвращает True,
         если точка выходит за пределы поля, и False, если не выходит."""
-        pass
+        return all([0 <= dot.x < len(self.board), \
+                    0 <= dot.y < len(self.board[0])])
 
     def shot(self):
         """

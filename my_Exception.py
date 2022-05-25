60f8ec210ad0b85050d7b597b2cedbdcaed1060b
Maksim Glazunov <magus.mgl@gmail.com>
2022-05-25 07:15:01 +0500

    Добавил исключения для класса Board

--- my_Exception.py
+++ my_Exception.py
@@ -1,21 +1,23 @@
-class BoardOutException(Exception):
-    """
-    Класс исключения, который  используется чтобы отлавливать ошибки,
-     когда игрок пытается выстрелить в клетку за пределами игрового поля.
-    """
+class BoardException(Exception):
+    def __init__(self, *args):
+        self.message = args
 
-    def __init__(self, dot):
-        self.dot = dot
+
+class BoardOutException(BoardException):
+    """Класс исключения, который  используется чтобы отлавливать ошибки, когда игрок пытается выстрелить в клетку за пределами игрового поля.
+    """
 
     def __str__(self):
-        return f"Точка с координатами ({self.dot[0]},{self.dot[1]})\
-         находится за пределами  игрового поля."
+        return f"Точка с координатами ({self.message[0]},{self.message[1]}) находится за пределами  игрового поля."
+
+
+class BoardOccupiedCage(BoardException):
+    def __str__(self):
+        return f"Точка с координатами ({self.message[0]},{self.message[1]}) занята."
 
 
 class DortCordsException(Exception):
-    """
-
-    """
+    """ """
 
     def __init__(self, *args):
         self.message = args[0] if args else None
@@ -30,24 +32,21 @@ class ShipParamException(Exception):
 
 
 class ShipLengthException(ShipParamException):
-    """
-    """
+    """ """
 
     def __str__(self):
         return f"Ошибка: длина коробля не может принимать значение '{self.message}'."
 
 
 class ShipLivesException(ShipParamException):
-    """
-    """
+    """ """
 
     def __str__(self):
         return f"Ошибка: Число жизней '{self.message}' должно принимать значение от 0 до 3."
 
 
 class ShipDirectionException(ShipParamException):
-    """
-    """
+    """ """
 
     def __str__(self):
         return f"Ошибка: Введенное положение корабля '{self.message}' должно принимать значение 'v'/'h'."

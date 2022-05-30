7065c08e492e05624a840284fa83b95fd0519c6c
Maksim Glazunov <magus.mgl@gmail.com>
2022-05-30 14:53:47 +0500

    рефач + метод shot для класса Board

--- game.py
+++ game.py
@@ -23,7 +23,7 @@ class Player():
             res = self.enemy_board.shot(grid_coordinate)
         except Exception as e:
             print(e)
-        return res == "hit"
+        return res
 
 
 class AI(Player):
@@ -35,12 +35,15 @@ class AI(Player):
 class User(Player):
     def ask(self):
         """Выбор случайной точки на доске"""
-        return Dot(map(int, input("Введите координаты клетки: ").split()))
+        x, y = list(map(int, input("Введите координаты клетки: ").split()))
+        return Dot(x, y)
 
 
 class Game:
     """Класс игры"""
-    SHIP_LENGTHS = [3, 2, 2, 1, 1, 1, 1]
+    SHIP_LENGTHS = [3]
+
+    # SHIP_LENGTHS = [3, 2, 2, 1, 1, 1, 1]
 
     def __init__(self):
         self.user_board = self._random_board()
@@ -54,26 +57,64 @@ class Game:
         """Метод генерирует случайную доску"""
         flag = True
         while flag:
-            new_board = Board(list_of_ships=[], number_of_live_ships=7)
+            new_board = Board(list_of_ships=[], number_of_live_ships=len(self.SHIP_LENGTHS))
             for ship_length in self.SHIP_LENGTHS:
                 for _ in range(1000):
                     try:
                         curr_ship = Ship(length=ship_length, start_dot=Dot(random.randint(0, 5), random.randint(0, 5)),
-                                         direction=random.randint(0, 1),
-                                         number_of_lives=ship_length)
+                                         direction=random.choice(["v", "h"]),
+                                         health=ship_length)
                         new_board.add_ship(curr_ship)
                     except Exception:
                         continue
                     else:
                         new_board.list_of_ships.append(curr_ship)
                         break
-            if len(new_board.list_of_ships) == 7:
+            if len(new_board.list_of_ships) == len(self.SHIP_LENGTHS):
                 flag = False
         return new_board
 
     def greet(self):
         """Метод, который в консоли приветствует пользователя и рассказывает о формате ввода"""
-        pass
+        str = """
+    Морской бой — игра для двух участников, в которой игроки по очереди называют координаты на 
+    неизвестной им карте соперника. Если у соперника по этим координатам имеется корабль (координаты заняты),
+     то корабль или его часть «топится», а попавший получает право сделать ещё один ход. 
+     Цель игрока — первым потопить все корабли противника.
+     
+     Игровое поле — квадрат 6×6 у каждого игрока, на котором размещается флот кораблей.
+     
+           | 1 | 2 | 3 | 4 | 5 | 6|
+
+        1 | О | О | О | О | О | О |
+
+        2 | О | О | О | О | О | О |
+
+        3 | О | О | О | О | О | О |
+
+        4 | О | О | О | О | О | О |
+
+        5 | О | О | О | О | О | О |
+
+        6 | О | О | О | О | О | О |
+
+
+    На поле размещаются:
+
+    1 корабль — ряд из 3 клеток («трёхпалубные»; крейсера)
+    2 корабля — ряд из 2 клеток («двухпалубные»; эсминцы)
+    4 корабля — 1 клетка («однопалубные»; торпедные катера)
+    
+    При размещении корабли не могут касаться друг друга сторонами и углами. 
+    
+    При попадании в корабль противника — на чужом поле ставится крестик (X), 
+    при холостом выстреле — буква T. Попавший стреляет ещё раз.
+    
+    Победителем считается тот, кто первым потопит все 7 кораблей противника.
+    
+    И так начнем игру. Расставим корабли на вашу доску и доску противника. 
+        """
+        print(str)
 
     def loop(self):
         """Метод с самим игровым циклом"""
@@ -85,9 +126,17 @@ class Game:
         self.loop()
 
 
-start = Game()
+new_game = Game()
+new_game.greet()
+new_game.ai_board.hid = False
 
-start.user_board.hid == True
-start.user_board.display_board()
-print()
-start.ai_board.display_board()
+# new_game.user_board.display_board()
+new_game.ai_board.display_board()
+
+# key = True
+# while key:
+#    new_game.player_user.move()
+#    new_game.ai_board.display_board()
+
+# if len(new_game.ai_board.list_of_ships) == 0:
+#     key = False
